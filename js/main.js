f58cb1a9be3bba6eb116a455099034cfa8af0ce2
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2020-03-03 09:12:33 -0300

    Creating new files of new design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
+const modal = document.getElementById("modalPj");
+const btn = document.getElementById("modalProj");
+const span = document.getElementsByClassName("close")[0];
+
+const modal2 = document.getElementById("modalConts");
+const btn2 = document.getElementById("modelCont");
+const span2 = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function() {
+	modal.style.display = "block";
+};
+
+span.onclick = function() {
+	modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
+
+btn2.onclick = function() {
+	modal2.style.display = "block";
+};
+
+span2.onclick = function() {
+	modal2.style.display = "none";
+};
+
+window.onclick = function(event) {
+	if (event.target == modal2) {
+		modal2.style.display = "none";
+	}
+};
--- js/projects.js
+++ js/projects.js
@@ -1,33 +1,20 @@
-ul_rep = $('#rep');
+const app = angular.module("myApp", []);
 
-function getRepo(data) {
-  return new Promise((resolve, reject) => {
-    $.ajax({ 
-      method: 'GET', 
-      url: 'https://api.github.com/users/ianbs/repos',
-      success: resolve, 
-      error: reject,
-      data
-    });
-  });
-}
-
-getRepo()
-.then((data) => {
-  for (i in data) {
-    ul_rep.append(
-      $('<li></li>')
-        .addClass('collection-item avatar')
-        .append($('<i></i>').addClass('material-icons circle').text('code'))
-        .append($('<span></span>').addClass('title').text(data[i].name))
-        .append($('<p></p>').text('Linguagem: ' + data[i].language))
-        .append($('<p></p>').text(data[i].owner.login))
-        .append($('<p></p>').text('Descrição: ' + data[i].description))
-        .append($('<p></p>').text('Criado em: ' + data[i].created_at))
-        .append($('<a></a>').attr('href', data[i].html_url).addClass('secondary-content').html("<i class='material-icons'>call_made</i>"))
-    );
-  }
-})
-.catch((err) => {
-  console.log('Erro', err);
-})
+app.controller("myCtrl", function($scope, $http, $element) {
+	$http
+		.get("https://api.github.com/users/ianbs/repos")
+		.then(function(response) {
+			const resp = response.data;
+			const div = $("#projects", $element);
+			resp.forEach(el => {
+				div.append(
+					`<a href='${el.html_url}' target='_blank'><div class='github'>
+              <p>Nome: ${el.name}</p>
+              <p>Owner: ${el.owner.login}</p>
+              <p>Description: ${el.description}</p>
+              <p>Language: ${el.language}</p>
+          </div></a>`
+				);
+			});
+		});
+});
